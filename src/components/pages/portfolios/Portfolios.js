4f47f25aa4c9339c8e6de9886f9d416c1d1fbf8a
Anubhav Pratihar <117383083+anubhavpratihar@users.noreply.github.com>
2023-05-19 18:51:57 +0530

    Update Portfolios.js

--- src/components/pages/portfolios/Portfolios.js
+++ src/components/pages/portfolios/Portfolios.js
@@ -4,300 +4,119 @@ export default function Portfolios() {
   return (
     <div className="card">
       <div className="card-content">
-        <h4 className="text-center"><strong>PORTFOLIO PROJECTS</strong></h4>
+        <h4 className="addPadding"><strong>PORTFOLIO</strong></h4>
 
-        <table className="portfolio-table">
-          <tbody>
-            <tr>
-              <th className="project-column"><h6>Tap to preview the Project on Sandbox</h6></th>
-              <th className="description-column"><h6>Description: The app may take some time to render 
-                as the sandbox will install dependencies based on the project. </h6></th>
-              <th className="git-column"><h6>Tab to redirect to the Git Repository</h6></th>
-            </tr>
-            <tr>
-              <td>
-                <a href="https://hyhsze.csb.app/">
-                  <h6>Google Keep App</h6>
-                </a>
-              </td>
-              <td>
-                <p>
-                  Note Taking App: Create, view, and delete notes with this simple React.js application. 
-                  Keep track of important information and stay organized. 
-                  Check out the app for a convenient way to manage your notes.
-                </p>
-              </td>
-              <td>
-                <p>
-                  <a href="https://github.com/anubhavpratihar/Keeper-App.git">
-                    <h6>Git Repository</h6>
-                  </a>
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <a className="x1 text-center" href="https://kdkp0k.csb.app/">
-                  <h6>Amazon Shopping App</h6>
-                </a>
-              </td>
-              <td>
-                <p>
-                  The React Shopping App is a web application developed using React.js.
-                  It provides users with a seamless shopping cart experience. The app features a dynamic loading screen, 
-                  a responsive navigation bar, and a cart container component for managing and displaying cart items. 
-                  Users can easily navigate through the app, add items to their cart, and proceed to checkout. 
-                  The app is built with modern web technologies such as React.js, JavaScript, HTML, and CSS. 
-                  It offers a user-friendly interface and aims to enhance the shopping experience for online customers.
-                </p>
-              </td>
-              <td>
-                <p>
-                  <a className="x1" href="https://github.com/anubhavpratihar/E-commerce.git">
-                    <h6>Git Repository</h6>
-                  </a>
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <a className="x1 text-center" href="https://n15dds.csb.app/">
-                  <h6>Youtube App</h6>
-                </a>
-              </td>
-              <td>
-                <p>
-                  The YouTube Video Search App is a React.js application that enables users to search for videos on YouTube and
-                  watch them directly within the app. It features a user-friendly interface with a search bar, a main video player, 
-                  and a list of related videos. Users can enter keywords or phrases to find videos, and the app fetches the search results using 
-                  the YouTube Data API. The selected video is displayed in the main player, while the related videos provide additional options for viewing.
-                  It provides a seamless and convenient way to explore and enjoy YouTube videos.
-                </p>
-              </td>
-              <td>
-                <p>
-                  <a className="x1" href="https://github.com/anubhavpratihar/yt.git">
-                    <h6>Git Repository</h6>
-                  </a>
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <a className="x1 text-center" href="https://0w1g4g.csb.app/">
-                  <h6>OTT App</h6>
-                </a>
-              </td>
-              <td>
-                <p>
-                  The Movie Search App is a React.js application that allows users to search for movies and
-                  add them to their favorites list. It uses the OMDB API to fetch movie data based on user search queries. 
-                  The app provides a user-friendly interface where users can browse and manage their favorite movies.
-                  It also includes features like removing movies from favorites and storing the favorites list in local 
-                  storage for persistence. The Movie Search App is built using React.js and Bootstrap for styling.
-                </p>
-              </td>
-              <td>
-                <p>
-                  <a className="x1" href="https://github.com/anubhavpratihar/ott.git">
-                    <h6>Git Repository</h6>
-                  </a>
-                </p>
-              </td>
-            </tr>
-
-
-
-
-            <tr>
-              <td>
-                <a className="x1 text-center" href="https://rlipz3.csb.app">
-                  <h6>Cocktail App</h6>
-                </a>
-              </td>
-              <td>
-                <p>
-                The Cocktail App is a React.js application that allows users to explore a wide range of cocktails.
-                 With its intuitive interface and seamless navigation powered by React Router, users can effortlessly 
-                 browse through the Home, About, SingleCocktail, and Error pages. Discover a diverse collection of cocktail
-                  recipes, view detailed information about each drink, and enjoy the convenience of a responsive Navbar for
-                   easy navigation. Whether you're a cocktail enthusiast or simply looking to expand your mixology 
-                   repertoire, the Cocktail App is your go-to resource for all things cocktails.
-                </p>
-              </td>
-              <td>
-                <p>
-                  <a className="x1" href="https://github.com/anubhavpratihar/cocktail.git">
-                    <h6>Git Repository</h6>
-                  </a>
-                </p>
-              </td>
-            </tr>
-
-
-            <tr>
-              <td>
-                <a className="x1 text-center" href="https://zt30n1.csb.app/>">
-                  <h6>To-Do-List App</h6>
-                </a>
-              </td>
-              <td>
-                <p>
-                The To-Do List App is a simple React.js application that helps users manage their tasks effectively.
-                 Users can add new tasks, mark tasks as completed, and delete tasks from the list. The app provides
-                  a user-friendly interface where tasks can be easily entered and managed. With dynamic rendering,
-                   the app ensures a seamless and responsive user experience. Start organizing your tasks with ease 
-                   using the To-Do List App!
-                </p>
-              </td>
-              <td>
-                <p>
-                  <a className="x1" href="https://github.com/anubhavpratihar/to-do-list.git">
-                    <h6>Git Repository</h6>
-                  </a>
-                </p>
-              </td>
-            </tr>
-
-
-
-
-            <tr>
-              <td>
-                <a className="x1 text-center" href="https://lbzd5f.csb.app/>">
-                  <h6>Rock Paper Scissor Game</h6>
-                </a>
-              </td>
-              <td>
-                <p>
-                The Rock Paper Scissors Game is a React.js application that allows users to play the classic
-                 game against the computer. It features a user interface where users can select their choice
-                  (rock, paper, or scissors) and see the computer's random choice. The game determines the outcome 
-                  of each round, updates the scores, and displays the turn result and final result. The game continues
-                   until a player reaches a score of 10. Players have the option to restart the game at any time. 
-                   The application is built using React.js, JavaScript, HTML, and CSS. Experience the nostalgic fun 
-                   of Rock Paper Scissors with this interactive web game!
-                </p>
-              </td>
-              <td>
-                <p>
-                  <a className="x1" href="https://github.com/anubhavpratihar/rock-paper-scissors.git">
-                    <h6>Git Repository</h6>
-                  </a>
-                </p>
-              </td>
-            </tr>
-
-
-            <tr>
-              <td>
-                <a className="x1 text-center" href="https://85isjz.csb.app/>">
-                  <h6>Candy Crush Game</h6>
-                </a>
-              </td>
-              <td>
-                <p>
-                Candy Crush Game: Swap candies to make matches and earn points in this React.js implementation. 
-                For the best experience, play on a laptop and enjoy the addictive gameplay.
-                </p>
-              </td>
-              <td>
-                <p>
-                  <a className="x1" href="https://github.com/anubhavpratihar/candy-crush.git">
-                    <h6>Git Repository</h6>
-                  </a>
-                </p>
-              </td>
-            </tr>
-
-
-
-            <tr>
-              <td>
-                <a className="x1 text-center" href="https://bxn8w3.csb.app/>">
-                  <h6>Resturant App</h6>
-                </a>
-              </td>
-              <td>
-                <p>
-                This React application represents a basic website structure with multiple pages.
-                 It includes a responsive Navbar and a Footer component that are displayed on every page. 
-                 The pages consist of Home, Menu, About, and Contact, each associated with a specific route using React
-                  Router. Users can navigate between pages by clicking on the corresponding links in the Navbar. 
-                  The styling is managed through the use of the App.css file.
-                </p>
-              </td>
-              <td>
-                <p>
-                  <a className="x1" href="https://github.com/anubhavpratihar/restaurant-app.git">
-                    <h6>Git Repository</h6>
-                  </a>
-                </p>
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                  <h6>Crypto Token</h6>
-              </td>
-              <td>
-                <p>
-                This React application integrates with the Internet Computer (ICP) platform and utilizes Motoko
-                as the backend language. It leverages the power of the Internet Computer's decentralized infrastructure
-                to handle authentication and user data management. The app securely authenticates users using 
-                Internet Identity (II) credentials, ensuring a reliable and trusted authentication process.
-                With Motoko as the backend language, developers can harness the capabilities of the Internet Computer
-                to build scalable and decentralized applications. The combination of React, Motoko, and ICP offers 
-                a powerful stack for creating secure and decentralized web applications on the Internet Computer platform.
-                </p>
-              </td>
-              <td>
-                <p>
-                  <a className="x1" href="https://github.com/anubhavpratihar/crypto-token.git">
-                    <h6>Git Repository</h6>
-                  </a>
-                </p>
-              </td>
-            </tr>
-
-
-            <tr>
-              <td>
-                  <h6>Open Sea NFT Marketplace ICP App</h6>
-              </td>
-              <td>
-                <p>
-                
-                The App component represents the frontend of an ICP (Internet Computer Protocol) project built with React.js.
-                 This application serves as a marketplace for NFTs (Non-Fungible Tokens), similar to OpenSea. 
-                 Users can browse, buy, and sell NFTs on this platform. The backend of the project is implemented in Motoko, 
-                 a language specifically designed for building decentralized applications on the Internet Computer.
-                  By combining React.js on the frontend and Motoko on the backend, this project creates a decentralized marketplace where users can interact with NFTs in a secure and scalable manner.
-
-
-
-                </p>
-              </td>
-              <td>
-                <p>
-                  <a className="x1" href="https://github.com/anubhavpratihar/Open-Sea-Nft-Marketplace.git">
-                    <h6>Git Repository</h6>
-                  </a>
-                </p>
-              </td>
-            </tr>
-
-
+        <ul className="x1 portfolio-list">
+          <li>
+            <a href="https://inquisitive-croquembouche-9e2b66.netlify.app">
+               <h6 className="noPadding">Google Keep App</h6>
+            </a>
+            <p>
+              This app is inspired by Google Keep, allowing users to create and manage their notes efficiently.</p>
+              <p> It provides a clean and intuitive interface for organizing thoughts, reminders, and to-do lists.</p>
             
-
-
-
-          </tbody>
-        </table>
+          </li>
+          <li>
+            <a className="x1" href="https://eloquent-dolphin-e0d996.netlify.app">
+               <h6 className="noPadding">Amazon Shopping App</h6>
+            </a>
+            <p>
+              Similar to the popular e-commerce platform, this app lets users browse and shop for various products.</p>
+              <p> It incorporates features like search, product listing, and a seamless checkout process.
+            </p>
+          </li>
+          <li>
+            <a className="x1" href="https://imaginative-puffpuff-5c236d.netlify.app">
+               <h6 className="noPadding">Youtube App</h6>
+            </a>
+            <p>
+              The Youtube App provides a user-friendly interface for searching and viewing videos. Users can watch their favorite videos,</p>
+              <p>explore trending content, and interact with the YouTube API.
+            </p>
+          </li>
+          <li>
+            <a className="x1" href="https://sage-lokum-30d2b4.netlify.app">
+               <h6 className="noPadding">OTT App</h6>
+            </a>
+            <p>
+              This app is designed to offer a personalized streaming experience, bringing together a collection of movies and TV shows.</p>
+              <p>It includes features like categories, recommendations, and a user-friendly player.
+            </p>
+          </li>
+          <li>
+            <a className="x1" href="https://tangerine-meringue-649b74.netlify.app">
+               <h6 className="noPadding">Cocktail App</h6>
+            </a>
+            <p>
+              If you're a cocktail enthusiast, this app is for you. It allows users to explore and discover a wide range of cocktail recipes.</p>
+              <p>With a simple interface, users can search for their favorite drinks and learn how to make them.
+            </p>
+          </li>
+          <li>
+            <a className="x1" href="https://warm-fenglisu-988100.netlify.app">
+               <h6 className="noPadding">To-Do-List App</h6>
+            </a>
+            <p>
+              Stay organized with this interactive to-do list app. It helps you manage your tasks efficiently by providing</p>
+              <p>features like adding, editing, and marking tasks as completed. Never forget an important task again!
+            </p>
+          </li>
+          <li>
+            <a className="x1" href="https://chimerical-blini-ed9789.netlify.app">
+               <h6 className="noPadding">Rock Paper Scissor Game</h6>
+            </a>
+            <p>
+              Put your hand-eye coordination to the test with this classic game. Challenge the computer and see if you can outsmart it</p>
+              <p>with your strategic choices of rock, paper, or scissors.
+            </p>
+          </li>
+          <li>
+            <a className="x1" href="https://preeminent-choux-287f56.netlify.app">
+               <h6 className="noPadding">Simon Game</h6>
+            </a>
+            <p>
+              Remember the Simon electronic game from the '80s? This app brings back the fun! Follow the pattern of lights</p>
+              <p>and sounds, and see how far you can go. It's a great way to test your memory skills.
+            </p>
+          </li>
+          <li>
+            <a className="x1" href="https://dashing-unicorn-ae6639.netlify.app">
+               <h6 className="noPadding">DrumKit App</h6>
+            </a>
+            <p>
+              Unleash your inner drummer with this virtual drum kit. Tap on the drums or use your keyboard to create</p>
+              <p> rhythmic beats. It's a perfect tool for practicing your drumming skills or just having some fun.
+            </p>
+          </li>
+          <li>
+            <a className="x1" href="https://fancy-pika-1a9d2c.netlify.app">
+               <h6 className="noPadding">Chat App</h6>
+            </a>
+            <p>
+              Connect with friends and family through this real-time chat application. Share messages, emojis,</p>
+              <p>and even multimedia files. It's a secure and convenient way to stay connected and engage in meaningful conversations.
+            </p>
+          </li>
+          <li>
+            <a className="x1" href="https://tiny-pegasus-51ce15.netlify.app">
+               <h6 className="noPadding">TinDog</h6>
+            </a>
+            <p>
+              Inspired by a popular dating app, TinDog helps dog lovers connect with each other. Swipe through</p>
+              <p>profiles, find matches, and arrange playdates for your furry friends. It's a fun and unique way to socialize.
+            </p>
+          </li>
+          <li>
+            <a href="https://github.com/anubhavpratihar?tab=repositories">
+               <h6 className="noPadding">Github Repository for all Projects.</h6>
+            </a>
+            <p>
+              Visit the GitHub repository to explore the source code, documentation, </p>
+              <p>and contributions made to these projects.
+            </p>
+          </li>
+        </ul>
       </div>
     </div>
   );
-  
 }
-
-
